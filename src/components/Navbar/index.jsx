f6924801ccb24c3905871a7f2f35c00a80902819
Hilola Umrzakova <khilolaumirzakova06@gmail.com>
2023-02-11 17:32:52 +0500

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import {  Outlet, Navigate } from 'react-router-dom'
+import {  Outlet , useNavigate } from 'react-router-dom'
 import { navbar } from '../../utils/navbar'
 import { Container, Section,Logo, Wrapper,Link } from './style'
 
 
 export const Home = () => {
+  const navigate=useNavigate();
   return (
     <Container>
       <Wrapper>
-      <Section onClick={() => Navigate('/home')}>
+      <Section logo onClick={() => navigate('/home')}>
        <Logo/>
-        <h3>Logo</h3>
+        <h3>Housing</h3>
       </Section>
       <Section>
        {
         navbar.map(({title,path},index)=>{
           return(
-            <Link key={index} to={path}>{title}</Link>
+            <Link className={({isActive}) => isActive && 'active'} key={index} to={path}>{title}</Link>
           )
         })
        } 
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -3,9 +3,13 @@ import styled from "styled-components";
 import { ReactComponent as logoImg} from '../../assets/icon/logo1.svg'
 
 export const Container =styled.div`
+/* display: flex;
+flex-direction: column;
+align-items: center; */
 
 `
 export const Wrapper =styled.div`
+/* max-width: 1440px; */
 background:var(--colorPrimary);
 color: #fff;
 padding:var(--padding);
@@ -18,7 +22,13 @@ align-items: center;
 
 export const Section=styled.div`
 display: flex;
-align-items: center;
+align-items: center; 
+cursor: ${({logo})=>logo&&'pointer'};
+
+.active{
+    color: red;
+ }
+
 `
 
 export const Logo=styled(logoImg)`
@@ -34,5 +44,6 @@ export const Link=styled(NavLink)`
 text-decoration: none;
 color: #ffff;
 padding: 0 30px;
-
+ 
 `
+
