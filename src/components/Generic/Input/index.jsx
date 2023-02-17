180cb360e0651fdf5039cb88996f0c2e58ef4559
Hilola Umrzakova <khilolaumirzakova06@gmail.com>
2023-02-17 14:09:24 +0500

    Generic button,input

---
+++ src/components/Generic/Input/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from './style'
+
+
+
+export const Input = ({children, type ,height ,onChange ,placeholder ,name ,value ,defaultValue}) => {
+  return (
+    <Container
+     placeholder={placeholder}
+     onChange={onChange}
+     name={name}
+     value={value}
+     defaultValue={defaultValue}
+     type={type}
+     height={height}>
+    {children}
+    </Container>
+  )
+}
+export default Input
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Button,Input} from '../Generic'
 import { Container } from './style'
 
 export const Home = () => {
   return (
-    <Container>Home</Container>
+    <Container>Home
+     <Button>Sending</Button>
+     <Input placeholder='Country'/>
+    </Container>
   )
 }
 
