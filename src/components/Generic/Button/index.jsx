b8cbf13408e1a8f53784343ee5d2381e942b8b04
Hilola Umrzakova <khilolaumirzakova06@gmail.com>
2023-02-22 14:36:48 +0500

    filter

--- src/components/Generic/Button/index.jsx
+++ src/components/Generic/Button/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Container } from './style'
 
-
-
 export const Button = ({children ,type ,height ,onClick}) => {
   return (
     <Container onClick={onClick} type={type} height={height}>{children || 'Generic Button'}</Container>
